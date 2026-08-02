de123855ec99e3c0a4214a085128e9123cb686fb
ahsadek <94584241+ahsadek@users.noreply.github.com>
2026-08-02 18:35:04 -0400

    Displaying window with openbox panel

--- GPA434Lab3DESolver/GPA434Lab3DESolver/GPA434Lab3DESolver.cpp
+++ GPA434Lab3DESolver/GPA434Lab3DESolver/GPA434Lab3DESolver.cpp
@@ -1,18 +1,93 @@
 #include "GPA434Lab3DESolver.h"
 
-GPA434Lab3DESolver::GPA434Lab3DESolver(QWidget *parent)
+#include <QHBoxLayout>
+#include <QIcon>
+#include <QSizePolicy>
+#include <QSplitter>
+#include <QVBoxLayout>
+#include <QWidget>
+
+#include "QDEBestResultPanel.h"
+#include "QDEControllerPanel.h"
+#include "QDEEngineParametersPanel.h"
+#include "QDEHistoryChartPanel.h"
+#include "QDEOpenBoxPanel.h"
+#include "QDEPeakPanel.h"
+#include "QDESolutionTabPanel.h"
+
+
+GPA434Lab3DESolver::GPA434Lab3DESolver(QWidget * parent)
     : QMainWindow(parent)
+    , mDEAdapter{}
+    , mControllerPanel{ new QDEControllerPanel(mDEAdapter) }
+    , mEngineParametersPanel{ new QDEEngineParametersPanel(mDEAdapter) }
+    , mSolutionTabPanel{ new QDESolutionTabPanel(mDEAdapter) }
+    , mBestResultPanel{ new QDEBestResultPanel(mDEAdapter) }
+    , mHistoryChartPanel{ new QDEHistoryChartPanel(mDEAdapter) }
 {
-    ui.setupUi(this);
+    mUi.setupUi(this);
 
-    setWindowTitle("Differential Evolution Solver");
-    setWindowIcon(QIcon(":/GPA434Lab3DESolver/dna-icon"));
-
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    // ajouter votre code à partir d'ici...
-    // ...
+    setupGui();
+    assembleInterface();
+    establishConnections();
+    addSolutionPanels();
 }
 
-GPA434Lab3DESolver::~GPA434Lab3DESolver()
+void GPA434Lab3DESolver::setupGui()
 {
+    setWindowTitle("Differential Evolution Solver");
+    setWindowIcon(QIcon(":/GPA434Lab3DESolver/dna-icon"));
+    resize(1200, 800);
+}
+
+void GPA434Lab3DESolver::assembleInterface()
+{
+    QWidget * controlWidget{ new QWidget };
+    controlWidget->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Preferred);
+
+    QVBoxLayout * controlLayout{ new QVBoxLayout(controlWidget) };
+    controlLayout->addWidget(mControllerPanel);
+    controlLayout->addWidget(mEngineParametersPanel);
+    controlLayout->addWidget(mBestResultPanel);
+
+    QSplitter * visualizationSplitter{ new QSplitter(Qt::Vertical) };
+    visualizationSplitter->addWidget(mSolutionTabPanel);
+    visualizationSplitter->addWidget(mHistoryChartPanel);
+    visualizationSplitter->setStretchFactor(0, 3);
+    visualizationSplitter->setStretchFactor(1, 1);
+
+    QHBoxLayout * contentLayout{ new QHBoxLayout };
+    contentLayout->addWidget(controlWidget);
+    contentLayout->addWidget(visualizationSplitter);
+
+    QVBoxLayout * mainLayout{ new QVBoxLayout(mUi.centralWidget) };
+    mainLayout->addLayout(contentLayout);
+}
+
+void GPA434Lab3DESolver::establishConnections()
+{
+    connect(mEngineParametersPanel, &QDEEngineParametersPanel::parameterChanged,
+            mControllerPanel, &QDEControllerPanel::resetSimulation);
+    connect(mEngineParametersPanel, &QDEEngineParametersPanel::parameterChanged,
+            mSolutionTabPanel, &QDESolutionTabPanel::updateVisualization);
+
+    connect(mSolutionTabPanel, &QDESolutionTabPanel::solutionChanged,
+            mEngineParametersPanel, &QDEEngineParametersPanel::setParametersFromSolution);
+    connect(mSolutionTabPanel, &QDESolutionTabPanel::solutionChanged,
+            mControllerPanel, &QDEControllerPanel::resetSimulation);
+
+    connect(mControllerPanel, &QDEControllerPanel::evolutionStarted,
+            mEngineParametersPanel, &QDEEngineParametersPanel::disable);
+    connect(mControllerPanel, &QDEControllerPanel::evolutionStopped,
+            mEngineParametersPanel, &QDEEngineParametersPanel::enable);
+    connect(mControllerPanel, &QDEControllerPanel::evolutionStarted,
+            mSolutionTabPanel, &QDESolutionTabPanel::disable);
+    connect(mControllerPanel, &QDEControllerPanel::evolutionStopped,
+            mSolutionTabPanel, &QDESolutionTabPanel::enable);
+}
+
+void GPA434Lab3DESolver::addSolutionPanels()
+{
+    // mSolutionTabPanel->addSolutionPanel(new QDEPeakPanel);
+    mSolutionTabPanel->addSolutionPanel(new QDEOpenBoxPanel);
 }
--- GPA434Lab3DESolver/GPA434Lab3DESolver/GPA434Lab3DESolver.h
+++ GPA434Lab3DESolver/GPA434Lab3DESolver/GPA434Lab3DESolver.h
@@ -1,17 +1,42 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+
+#include "QDEAdapter.h"
 #include "ui_GPA434Lab3DESolver.h"
 
+class QDEBestResultPanel;
+class QDEControllerPanel;
+class QDEEngineParametersPanel;
+class QDEHistoryChartPanel;
+class QDESolutionTabPanel;
 
+
+//! \brief Main window assembling the differential evolution application.
 class GPA434Lab3DESolver : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    GPA434Lab3DESolver(QWidget *parent = nullptr);
-    ~GPA434Lab3DESolver() override;
+    GPA434Lab3DESolver(QWidget * parent = nullptr);
+    GPA434Lab3DESolver(GPA434Lab3DESolver const &) = delete;
+    GPA434Lab3DESolver(GPA434Lab3DESolver &&) = delete;
+    GPA434Lab3DESolver& operator=(GPA434Lab3DESolver const &) = delete;
+    GPA434Lab3DESolver& operator=(GPA434Lab3DESolver &&) = delete;
+    ~GPA434Lab3DESolver() override = default;
 
 private:
-    Ui::GPA434Lab3DESolverClass ui;
+    QDEAdapter mDEAdapter;
+    Ui::GPA434Lab3DESolverClass mUi;
+
+    QDEControllerPanel * mControllerPanel{};
+    QDEEngineParametersPanel * mEngineParametersPanel{};
+    QDESolutionTabPanel * mSolutionTabPanel{};
+    QDEBestResultPanel * mBestResultPanel{};
+    QDEHistoryChartPanel * mHistoryChartPanel{};
+
+    void setupGui();
+    void assembleInterface();
+    void establishConnections();
+    void addSolutionPanels();
 };
